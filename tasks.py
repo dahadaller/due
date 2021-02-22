8e91a2521170c69dac23868797d8630f521035ca
David Hadaller <dahadaller@gmail.com>
2021-02-21 20:03:30 -0500

    serialize/deserialize tasks TODO

--- tasks.py
+++ tasks.py
@@ -1,15 +1,29 @@
+# TODO: serialize data to json with default function for json.dump
+    # https://stackoverflow.com/a/41200652/7215135
+# TODO: create in-memory milestone list with subtasks (without deferments) and
+    # serialize to json using "default" function argument in json.dumps
+# TODO: find out how to deserialize json data into a milestone list complete with
+    # task objects and deferment objects 
+
+
 class Milestones:
     """docstring"""
 
-    # TODO: find out how to parse the json dictionary of self.data into
-    # task objects and deferment objects and then return a list of the top-level
-    # task objects (milestones) with all the subtasks (task objects) and
-    # deferment objects instantiated
-    def __init__(self, path):
+    def __init__(self, path, task_list):
 
-        # TODO:  Flashcard for import json into dict
-        with open(path, "r") as read_file:
-            self.data = json.load(read_file)
+        if not path:
+            self.data = task_list
+        else:
+            # TODO:  Flashcard for import json into dict
+
+            with open(path, "r") as read_file:
+                self.data = json.load(read_file)
+
+
+    def to_json(self,path):
+        with open(path, "w") as write_file:
+            json.dump(self.data, write_file)
+
 
 class Task:
     """docstring"""
@@ -23,22 +37,23 @@ class Task:
         self.sub_tasks = {}
 
 
-class Deferment:
-    """docstring"""
-
-    def __init__(self, deferment_date):
-        self.date = deferment_date
-        self.from = None
-        self.to =  None
-        self.category = None
-        self.justification = None
-
-
-
-    # edit this to print the task and all subtasks in a really pretty way.
-    # https://realpython.com/operator-function-overloading/#printing-your-objects-prettily-using-str
-    def __str__(self):
-        pass
+#
+# class Deferment:
+#     """docstring"""
+#
+#     def __init__(self, deferment_date):
+#         self.date = deferment_date
+#         self.from = None
+#         self.to =  None
+#         self.category = None
+#         self.justification = None
+#
+#
+#
+#     # edit this to print the task and all subtasks in a really pretty way.
+#     # https://realpython.com/operator-function-overloading/#printing-your-objects-prettily-using-str
+#     def __str__(self):
+#         pass
 
 
 # DISPAY INTERFACE
@@ -47,7 +62,7 @@ class Deferment:
     # TODO: SHOW TODAY'S TASKS: generate a list of all tasks for today, sorted by milestone
     # TODO:export to markdown for all of these views
 
-    
+
 # TODO: display an "error log" for each deferment as follows:
     # list summary statistics for each category of deferment
     # milestone_name
