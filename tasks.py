f5d24c739d7ddc8e4f5271844d5843820bb089dd
David Hadaller <dahadaller@gmail.com>
2021-03-18 16:52:31 -0400

    Subtasks can now be added to tasks via method call, and tasks can be created from and converted to dictionaries

--- tasks.py
+++ tasks.py
@@ -3,12 +3,12 @@ import json
 class Task:
     """docstring"""
 
-    def __init__(self, task_id, task_name, deadline=None, deadline_changes = {}, subtasks = [], complete=False):
+    def __init__(self, task_name, task_id='root', deadline=None,complete=False, deadline_changes = {}, subtasks = []):
 
-        self.task_id = task_id
+        self.id = task_id
         self.name = task_name
         self.deadline = deadline
-        self.deadline_changes = deadline_changes
+        self.deadline_changes = {k:v for k,v in deadline_changes.items()}
         self.complete = complete
 
 
@@ -22,7 +22,7 @@ class Task:
                         task_id = subtask['task_id'],
                         task_name = subtask['task_name'],
                         deadline = subtask['deadline'],
-                        deadline_changes = {k:v for k,v in subtask['deadline_changes'].items()},
+                        deadline_changes = subtask['deadline_changes'],
                         subtasks = subtask['subtasks'],
                         complete = subtask['complete']
                     )
@@ -32,12 +32,23 @@ class Task:
             self.subtasks = subtasks
 
     @staticmethod
+    def from_dict(task_dict):
+        return Task(
+            task_id = task_dict.get('task_id','root'),
+            task_name = task_dict.get('task_name','root'),
+            deadline = task_dict.get('deadline',None),
+            complete = task_dict.get('complete',False),
+            deadline_changes = task_dict.get('deadline_changes',{}),
+            subtasks = task_dict.get('subtasks',[]),
+        )
+
+    @staticmethod
     def from_json(file_path):
         with open(file_path, "r") as read_file:
-            t = Task(task_id='root',
+            return Task(
                 task_name='root',
-                subtasks= json.load(read_file))
-        return t
+                subtasks= json.load(read_file)
+            )
 
     def to_string(self, indent=0, max_depth=None):
         if max_depth is not None:
@@ -52,7 +63,7 @@ class Task:
         checkbox = "☑" if self.complete else "☐"
         output = "{indents}{complete} {deadline} {task_id} {task_name}\n{subtasks}".format(
             indents = indents,
-            task_id = self.task_id,
+            task_id = self.id,
             task_name =  self.name,
             complete = checkbox,
             deadline = self.deadline,
@@ -60,28 +71,23 @@ class Task:
         )
         return output
 
+    def to_dict(self):
+        return {
+            'task_id': self.id,
+            'task_name': self.name,
+            'deadline': self.deadline,
+            'deadline_changes': self.deadline_changes,
+            'complete':self.complete,
+            'subtasks':  [s.to_dict() for s in self.subtasks]
+        }
 
-    def __repr__(self):
-        if self.name == 'root' and self.task_id=='root':
+
+    def __str__(self):
+        if self.name == 'root' and self.id=='root':
             output = ''.join([s.to_string() for s in self.subtasks])
         else:
             output = self.to_string()
         return output
-
-    # TODO: revise to allow for decimal task id's
-    # def add_subtask(self, task_name, deadline=None, deadline_changes = {}, subtasks = [], complete=False):
-    #     prev_task_id = self.subtasks[-1]['task_id']
-    #     new_task_id = Task.TASK_IDs[Task.TASK_IDs.find(prev_task_id) + 1]
-    #     self.subtasks.append(
-    #         Task(
-    #             task_id = new_task_id,
-    #             task_name = task_name,
-    #             deadline=deadline,
-    #             deadline_changes=deadline_changes,
-    #             subtasks=subtasks,
-    #             complete=complete
-    #         )
-    #     )
 
     def filter_by_id(self, task_id):
 
@@ -101,7 +107,7 @@ class Task:
         if not self.subtasks:
             if callback(self):
                 return Task(
-                    task_id = self.task_id,
+                    task_id = self.id,
                     task_name = self.name,
                     deadline = self.deadline,
                     deadline_changes = self.deadline_changes,
@@ -124,7 +130,7 @@ class Task:
                 return None
             else:
                 return Task(
-                    task_id = self.task_id,
+                    task_id = self.id,
                     task_name = self.name,
                     deadline = self.deadline,
                     deadline_changes = self.deadline_changes,
@@ -133,7 +139,18 @@ class Task:
                 )
 
 
+    def next_id(self):
+        id = self.id
+        id = id.rpartition('.')
+        id[-1] = int(id[-1]) + 1
+        id[-1] = str(id[-1])
+        id = ''.join(id)
+        return id
 
+    def add_subtask(self, subtask):
+        print(self.subtasks)
+        subtask.task_id = self.subtasks[-1].next_id()
+        self.subtasks.append(subtask)
 
 
 
@@ -160,9 +177,16 @@ class Task:
 # TODO: implement function to filter deferments by category
 
 t = Task.from_json('todo.json')
-# print(t)
-# print(t.filter_by_id('0'))
-print(t.filter(lambda x: x.name.startswith('sec'))) # filter by task name
-print(t.filter(lambda x: x.deadline <= '2021-04-03')) #filter by deadline
-
+print(t)
+print(Task.from_dict(t.to_dict()))
+# t.add_subtask(
+#     Task(
+#         task_name =  'do the thing',
+#         deadline  = '2021-03-18'
+#     )
+# )
+# # print(t.filter_by_id('0'))
+# print(t.filter(lambda x: x.name.startswith('sec'))) # filter by task name
+# print(t.filter(lambda x: x.deadline <= '2021-04-03')) #filter by deadline
+#
 # print(t)
